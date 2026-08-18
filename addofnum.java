74dcadac7b16fde3feb2fb7514c33b876be593b7
Jagadeesh250151-eng <jagadeesh.250151@cse.ritchennai.edu.in>
2026-08-18 19:51:58 +0530

    Added addofnum program

---
+++ addofnum.java
@@ -0,0 +1,13 @@
+package jagaproject;
+
+public class addofnum {
+
+	public static void main(String[] args) {
+		int a=32;
+		float b = 32.5f;
+		System.out.println(a+b);// TODO Auto-generated method stub
+
+	}
+
+}
+
