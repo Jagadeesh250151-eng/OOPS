2a3188707acb365698c245ca535c3ca73addbae2
JAGADEESH D L <jagadeesh.250151@cse.ritchennai.edu.in>
2026-08-19 18:12:21 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,13 @@
+package jagaproject;
+
+public class loop {
+
+	public static void main(String[] args) {
+		int N=10;
+		System.out.println("Numbers from 1 to 10");
+		for(int i=1;i<=N;i++)
+			System.out.println(i);// TODO Auto-generated method stub
+
+	}
+
+}
