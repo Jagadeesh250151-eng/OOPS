2fba9aa26049e197716320567a80542a3d106f1f
JAGADEESH D L <jagadeesh.250151@cse.ritchennai.edu.in>
2026-08-19 18:21:36 +0530

    Add files via upload

---
+++ mydatatypes.java
@@ -0,0 +1,18 @@
+package jagaproject;
+
+public class mydatatypes {
+
+	public static void main(String[] args) {
+		String name="Jhon";
+		char che='a';
+		boolean a=true;
+		int b=25;
+		float c=45.5f;
+		double d=45.55d;
+		System.out.println(name);
+		System.out.println(a);
+		// TODO Auto-generated method stub
+
+	}
+
+}
