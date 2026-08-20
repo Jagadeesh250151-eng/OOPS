a233f62d8ee1dfa4ee8129551e0085e1100b40e3
JAGADEESH D L <jagadeesh.250151@cse.ritchennai.edu.in>
2026-08-20 19:04:46 +0530

    Add files via upload

---
+++ myoperators.java
@@ -0,0 +1,16 @@
+package jagaproject;
+
+public class myoperators {
+
+	public static void main(String[] args) {
+		int a=5+10;
+		int m=5;
+		m*=5;
+		System.out.println(a);
+		System.out.println(m);
+		System.out.println(a<m||a>10);
+		
+
+	}
+
+}
