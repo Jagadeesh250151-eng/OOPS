6ccbc5f7f2692fd055e7397eb4fdc214fd20a990
JAGADEESH D L <jagadeesh.250151@cse.ritchennai.edu.in>
2026-08-21 09:40:05 +0530

    Add files via upload

---
+++ sumofnumbers.java
@@ -0,0 +1,16 @@
+package jagaproject;
+import java.util.Scanner;
+public class sumofnumbers {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int n=sc.nextInt();
+		int sum=0;
+		for(int i=1;i<=n;i++)
+			sum=sum+i;
+		System.out.println("Sum="+sum);
+
+	}
+
+}
