51e33a4a0bfb0ad9fcf3953dbb9e75f5b15634a2
JAGADEESH D L <jagadeesh.250151@cse.ritchennai.edu.in>
2026-08-24 19:04:46 +0530

    Add files via upload

---
+++ Divisor.java
@@ -0,0 +1,17 @@
+package jagadeesh2117250020151;
+import java.util.Scanner;
+
+public class Divisor {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println("Divisors:");
+		for(int i=2;i<n;i++)
+			if(n%i==0)
+				System.out.print(i+"  ");
+		sc.close();
+
+	}
+
+}
