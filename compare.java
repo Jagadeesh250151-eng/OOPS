e44ebe1e89e961265de1ff13fad74da33d571291
JAGADEESH D L <jagadeesh.250151@cse.ritchennai.edu.in>
2026-08-25 19:11:12 +0530

    Add string comparison functionality in Hello class

---
+++ compare.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+
+        if (str1.equals(str2)) {
+            System.out.print("Yes");
+        } else {
+            System.out.print("No");
+        }
+    }
+}
