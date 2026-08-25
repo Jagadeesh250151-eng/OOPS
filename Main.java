032e19fb7fd856343692bde8d4993f0ccbd8b38e
JAGADEESH D L <jagadeesh.250151@cse.ritchennai.edu.in>
2026-08-25 19:06:50 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package jagadeesh2117250020151;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println("Sum="+c);
+		
+	}
+
+}
