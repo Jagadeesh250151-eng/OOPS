458b07b481f3ef87aecaa5de4ba41202e6590c04
JAGADEESH D L <jagadeesh.250151@cse.ritchennai.edu.in>
2026-08-26 20:54:11 +0530

    Add Hello class to print a string from char array

---
+++ strarr.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char[] arr = {'s', 'k', 'i', 'l', 'l'};
+        String str = new String(arr);
+
+        System.out.print(str);
+    }
+}
