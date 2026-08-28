03f07c6d0ef8485beea98fe32b41d32c8f1dffdb
JAGADEESH D L <jagadeesh.250151@cse.ritchennai.edu.in>
2026-08-28 18:57:43 +0530

    Add Main class with strMethod for greeting

---
+++ liam.java
@@ -0,0 +1,11 @@
+public class Main {
+  static void strMethod(String fname) {
+    System.out.println(fname + " Refsnes");
+  }
+
+  public static void main(String[] args) {
+    strMethod("Liam");
+    strMethod("Jenny");
+    strMethod("Anja");
+  }
+}
