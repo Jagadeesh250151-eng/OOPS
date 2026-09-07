e936796e6cfb4e28d5df51a4d24cc92d97857627
JAGADEESH D L <jagadeesh.250151@cse.ritchennai.edu.in>
2026-09-07 22:15:53 +0530

    Add College class with static method to display name

---
+++ static.java
@@ -0,0 +1,13 @@
+class College {
+    static String collegeName = "RIT";
+
+    static void displayCollege() {
+        System.out.println("College: " + collegeName);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        College.displayCollege();
+    }
+}
