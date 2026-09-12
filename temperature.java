dc9030fbad37f2695f1f76bf2b27749e14aa58ba
JAGADEESH D L <jagadeesh.250151@cse.ritchennai.edu.in>
2026-09-12 21:29:05 +0530

    Add Temperature class for Celsius to Fahrenheit conversion

---
+++ temperature.java
@@ -0,0 +1,20 @@
+class Temperature {
+    double celsius;
+
+    Temperature(double c) {
+        celsius = c;
+    }
+
+    void convert() {
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        System.out.println("Celsius: " + celsius);
+        System.out.println("Fahrenheit: " + fahrenheit);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Temperature t = new Temperature(25);
+        t.convert();
+    }
+}
